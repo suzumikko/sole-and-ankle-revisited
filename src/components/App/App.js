15d554d2e975a19e52529448c46f4c6f445dfefc
suzumikko <sandra.preysing@zoho.com>
2022-03-07 08:14:42 +0100

    solution exercise 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-
+import { ThemeProvider } from 'styled-components';
+import { Theme, QUERIES } from '../../constants';
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
 
@@ -8,17 +9,28 @@ const App = () => {
   const [sortId, setSortId] = React.useState('newest');
 
   return (
-    <>
+    <ThemeProvider theme={Theme}>
       <Header />
       <Main>
         <ShoeIndex sortId={sortId} setSortId={setSortId} />
       </Main>
-    </>
+    </ThemeProvider>
   );
 };
 
 const Main = styled.main`
   padding: 64px 32px;
+  /*
+  @media ${(p) => p.theme.queries.laptopAndDown} {
+    background-color: green;
+  }
+  @media ${QUERIES.tabletAndDown} {
+    background-color: blue;
+  }
+  @media ${QUERIES.phoneAndDown} {
+    background-color: beige;
+  }
+  */
 `;
 
 export default App;

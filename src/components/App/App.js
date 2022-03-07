236b7036a0fb4feac1bc219839cf923c69f05123
suzumikko <sandra.preysing@zoho.com>
2022-03-07 16:10:28 +0100

    solution exercise 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,6 +20,15 @@ const App = () => {
 
 const Main = styled.main`
   padding: 64px 32px;
+
+  @media ${QUERIES.tabletAndDown} {
+    padding: 48px 32px;
+  }
+
+  @media ${QUERIES.phoneAndDown} {
+    padding: 48px 16px;
+  }
+
   /*
   @media ${(p) => p.theme.queries.laptopAndDown} {
     background-color: green;
